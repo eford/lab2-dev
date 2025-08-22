6edcda6afffa505ecdc78eaf440b6d930ffd62d2
Eric Ford <eford@psu.edu>
2025-08-22 05:40:38 -0400

    updating Project

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,19 @@
+#println("Making sure Pluto is installed")
+import Pkg; 
+#Pkg.add(name="Pluto", version="0.20.4");
+import Pluto; 
+
+println("Installing packages for ex1.jl")
+Pluto.activate_notebook_environment("../ex1.jl"); 
+Pkg.instantiate(); 
+
+println("Installing packages for ex2.jl")
+Pluto.activate_notebook_environment("../ex2.jl"); 
+Pkg.instantiate(); 
+
+println("Preventing generating html for ex3 everytime.")
 println(basename(pwd()))
-if !occursin(r"lab2-start",pwd())
+if !occursin(r"lab2$",pwd())
    println("# Writing PlutoDeployment to avoid exporting ex3.jl everytime.")
    settings = 
    """
